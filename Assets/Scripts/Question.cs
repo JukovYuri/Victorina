69aab5f05860daf17628c2d12092008983ff011b
JukovYuri <jukov_yuri@mail.ru>
2020-10-14 01:03:55 +0300

    111

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Victorina/Question", fileName ="Q_")]
+public class Question : ScriptableObject
+{
+    public Sprite sprite;
+
+    [Space(15)] [TextArea(15, 50)] public string contentQuestion;
+    [Space(15)] public int numberTrueAnswer;
+    [Space(15)] [TextArea(5, 10)] 
+    public string [] contentPossibleAnswers = new string [4];
+    
+
+
+}
---
+++ Assets/Scripts/Victorina.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Victorina : MonoBehaviour
+{
+    public Text infoQuestionsAnswers;
+    public Text infoTimer;
+    public Text infoLife;
+    [Space(15)]
+    public Image image;
+    public Text contentQuestion;
+    public Text[] txtAnswers;
+    public Button[] btnAnswers;
+    [Space(15)]
+    public Button fiftyfifty;
+    public Button plus20;
+    public Button help;
+    [Space(15)]
+    public Question[] arrayQuestions;
+    [Space(15)]
+    public Question currentQuestion;
+
+    void Start()
+    {
+        //запуск таймера
+
+        
+        //пересортировать массив
+
+        
+
+
+    }
+
+    void Update()
+    {
+        //проверка на правильность
+        //изменение инфо игры
+    }
+
+    void GetQuestions (int i) {
+
+        currentQuestion = arrayQuestions[i];
+
+        contentQuestion.text = currentQuestion.contentQuestion;
+        image.sprite = currentQuestion.sprite;
+
+
+        for (int j = 0; j < currentQuestion.contentPossibleAnswers[j].Length; j++)
+        {
+            txtAnswers[j].text = currentQuestion.contentPossibleAnswers[j];
+        }
+    }
+
+    void CheckAnswer()
+    {
+
+    }
+}
